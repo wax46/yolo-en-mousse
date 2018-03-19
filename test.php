5c9d4dd15653d76167a00804561996942723ef7f
max <wax46@hotmail.fr>
2018-03-19 15:35:49 +0100

    starter
    template yolo start

---
+++ test.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>Site Max</title>
+
+
+</head>
+<body>
+
+<h1>yeah grand</h1>
+
+<p>git c'est d'la balle !!</p>
+
+
+
+</body>
+</html>
